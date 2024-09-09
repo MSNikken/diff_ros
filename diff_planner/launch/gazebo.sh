ec4b572149e9f4b58a4fd74e5010360ce4ff8364
michiel <m.s.nikken@student.utwente.nl>
2024-09-09 16:20:00 +0200

    Update link to wandb

--- diff_planner/launch/gazebo.sh
+++ diff_planner/launch/gazebo.sh
@@ -5,4 +5,4 @@ cd ~/Documents/MTP/code/catkin_ws || exit
 source src/venv/bin/activate
 source devel/setup.bash
 
-roslaunch diff_planner gazebo.launch wandb_file:=dl_rob/diffusion/gcf9cf3d
+roslaunch diff_planner gazebo.launch wandb_file:=dl_rob/diffusion/5t0sxiys
